d1cab4aac8f610b7548cc10aaf8992ce06368976
Kemran Karimov <kemran.karimov@gmail.com>
2023-01-17 15:52:21 +0600

    hotfixes: button image add, button background remove

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,8 @@ import styles from './styles.module.scss';
 import {FC} from "react";
 import cn from 'classnames'
 import {HeaderMenu} from "@/components/HeaderMenu";
+import burgerButtonIcon from '../../../public/images/menu-burger-horizontal-svgrepo-com .svg';
+import closeButtonIcon from '../../../public/images/cancel-close-svgrepo-com.svg';
 const bodyScrollLock = require('body-scroll-lock');
 
 interface IHeader {
@@ -34,12 +36,16 @@ export const Header: FC<IHeader> = ({ isOpen, setIsOpen }) => {
                 </h1>
                 <button
                     className={cn(
-                        styles['menu-burger-button'],
-                        { [styles['menu-burger-button-open']]: !isOpen },
-                        { [styles['menu-burger-button-close']] : isOpen }
+                        styles['menu-button'],
+                        { [styles['menu-button-close']] : isOpen }
                     )}
                     onClick={toggleNavMenuOpen}
                 >
+                    <img
+                        className={styles['menu-button-icon']}
+                        src={isOpen ? closeButtonIcon.src : burgerButtonIcon.src}
+                        alt={'button icon'}
+                    />
                 </button>
                 { isOpen && <HeaderMenu setIsOpen={toggleNavMenuOpen}/> }
                 <nav className={styles.navigation}>

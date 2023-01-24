8d6750aee2a27a481528eca2307a4c2184c0765e
Kemran Karimov <kemran.karimov@gmail.com>
2023-01-24 12:34:16 +0600

    feature: marketplace structure and markup

--- src/components/Marketplace/index.tsx
+++ src/components/Marketplace/index.tsx
@@ -1,19 +1,23 @@
 import styles from './styles.module.scss';
 import { CarCard } from '@components/CarCard';
 import Link from 'next/link';
-import {carsData} from "@components/Marketplace/constants";
+import { carsData } from '@components/Marketplace/constants';
 
 export const Marketplace = () => {
   return (
     <div className={styles['marketplace']}>
       <div className={styles['wrapper']}>
         <h1 className={styles['marketplace__title']}>Результаты поиска</h1>
-        <div className={styles['markeplace__content']}>
+        <div className={styles['marketplace__content']}>
           <div className={styles['marketplace__filter']}></div>
           <div className={styles['marketplace__grid']}>
             {carsData.map((item) => {
               return (
-                <Link href={'#'} className={styles['marketplace__link']} key={item.id}>
+                <Link
+                  href={'#'}
+                  className={styles['marketplace__link']}
+                  key={item.id}
+                >
                   <CarCard
                     image={item.image}
                     make={item.make}

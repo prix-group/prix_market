65314527255dce686f2a738220159481c28e9c96
Daniyar Otynshin <daniyar.otynshin@gmail.com>
2023-01-26 20:57:33 +0600

    fix: put head component back

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,8 @@
-import { Html, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
+      <Head />
       <body>
         <Main />
         <NextScript />
